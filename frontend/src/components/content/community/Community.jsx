fdfc97a7d12a7df4b4d969c20625afce8bfbfb3f
xolooh <tapsukk@hotmail.fi>
2024-04-10 11:38:47 +0300

    aasd

--- frontend/src/components/content/community/Community.jsx
+++ frontend/src/components/content/community/Community.jsx
@@ -4,12 +4,14 @@ import UserList from './Userlist';
 import AllGroups from './AllGroups';
 
 const Community = () => {
+  
 
   return (
 
     <div className="content">
       <h2>Yhteisö</h2>
       Löydä palvelumme käyttäjät, ryhmät ja arvostelut täältä.
+      
 
       <UserList />
       <AllGroups />
